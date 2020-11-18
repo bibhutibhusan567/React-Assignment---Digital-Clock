67d78382a0892ff3d266d6b9cc84aced40a69be4
bibhutibhusan567 <bibhutibhusan567@gmail.com>
2020-11-18 21:26:14 +0530

    lets run

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,46 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import '../styles/App.css';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: new Date()
+        };
+        this.intervalId = null;
+    }
     render() {
-
-        return(
-            <>
-               
-            </>
+        return (
+            <div className="Clock">
+                <h3 id="time">{this.getTimeString()}</h3>
+            </div>
         )
+    }
+
+    componentDidMount() {
+        this.intervalId = setInterval(() => {
+            this.setState({ time: new Date() });
+        }, 1 * 1000);
+    }
+
+    getTimeString() {
+        const currTime = this.state.time;
+        const [hours, minutes, seconds] = [currTime.getHours(), currTime.getMinutes(), currTime.getSeconds()];
+
+        const amOrPm = hours >= 12 ? "Pm" : "Am";
+        const twelveHourFormat = hours > 12 ? (hours - 12) : hours;
+        const hourString = " " + twelveHourFormat;
+        const minuteString = this.padNumberToTwoDigit(minutes);
+        const secondString = this.padNumberToTwoDigit(seconds);
+
+        const timeString = `${hourString}:${minuteString}:${secondString} ${amOrPm}`;
+
+        return timeString;
+    }
+    padNumberToTwoDigit(num) {
+        //return (num < 10 ? ("0" + num) : ("" + num));
+        //return ((num < 10 ? "0" : "") + num);
+        return `${num < 10 ? "0" : ""}${num} `;
     }
 }
 
